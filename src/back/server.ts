1823f9c150a01dd66d3267a0d0a1b43da5c2e212
Kleber Rodrigues <kleb3r.jr@gmail.com>
2024-01-25 17:42:00 -0300

    reset

--- src/back/server.ts
+++ src/back/server.ts
@@ -4,13 +4,12 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 
-
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+  res.sendFile(join(__dirname, '../front/index.html'));
 });
 
 io.on('connection', (socket) => {
