ea0136bb12629720ab8aa4213d1dfc296d638a7a
Kleber Rodrigues <kleb3r.jr@gmail.com>
2024-02-20 11:47:23 -0300

    feat: add cookie parser middleware

--- src/registerMiddlewares.ts
+++ src/registerMiddlewares.ts
@@ -1,7 +1,9 @@
+import cookieParserMiddleware from 'cookie-parser';
 import { authenticationMiddleware } from "./middlewares/authenticationMiddleware";
 import { testMiddleware } from "./middlewares/testMiddleware";
 
 export const registerMiddlewares = (app: any) => {
+  app.use(cookieParserMiddleware());
   app.use(authenticationMiddleware);
   app.use(testMiddleware);
 }
