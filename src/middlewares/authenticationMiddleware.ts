757e0030a230ec7dcd466dcc6304d74800d5f386
Kleber Rodrigues <kleb3r.jr@gmail.com>
2024-02-20 11:47:47 -0300

    feat: login and authentication system v1

--- src/middlewares/authenticationMiddleware.ts
+++ src/middlewares/authenticationMiddleware.ts
@@ -1,8 +1,28 @@
-export const authenticationMiddleware = (req, res, next) => {
+import { NextFunction, Request as Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import getFromEnv from "../utils/env.utils";
+
+export const authenticationMiddleware = (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
   console.log('>>> Request passing through Authentication Middleware...');
   
   if (req.path === '/login') {
     return next();
+  }
+
+  const sessionToken = req.cookies.session;
+  
+  if (!sessionToken) {
+    throw new Error('Missing cookie');
+  }
+
+  try {
+    jwt.verify(sessionToken, getFromEnv('SECRET'));
+  } catch (e) {
+    throw new Error('Invalid token');
   }
 
   return next();
--- src/registerRoutes.ts
+++ src/registerRoutes.ts
@@ -1,7 +1,45 @@
 import { join } from "path";
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import { Request, Response } from "express";
+import { daysToMiliseconds } from "./utils/date.utils";
 
 export const registerRoutes = (app: any) => {
-  app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+  app.get('/', (req: Request, res: Response) => {
+    res.sendFile(join(__dirname, '/pages/index.html'));
+  });
+
+  app.post('/login', async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    console.log('>>> email:', email);
+    console.log('>>> password:', password);
+
+    const prisma = new PrismaClient();
+    const user = await prisma.user.findUnique({
+      where: { email, password }
+    });
+
+    if (!user) {
+      throw new Error('Wrong credentials');
+    }
+
+    const token = jwt.sign(
+      { email, password },
+      process.env.SECRET ?? ''
+    );
+  
+    res.cookie('session', token, {
+      maxAge: daysToMiliseconds(2), 
+      secure: process.env.ENV === 'PROD',
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+
+    res.send({ message: 'success' });
+  });
+
+  app.post('/logout', async (req: Request, res: Response) => {
+    res.clearCookie('session');
+    res.send({ message: 'sucess' });
   });
 }
