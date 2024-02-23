fc740242f4014473360f6cc80124ac2de0cedd0d
Kleber Rodrigues <kleb3r.jr@gmail.com>
2024-02-22 21:51:49 -0300

    refactor: initialize middleware inside proper function

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ try {
   const app = express();
   const httpServer = createServer(app);
   
-  app.use(express.json());
   registerMiddlewares(app);
   registerRoutes(app);
 
--- src/registerMiddlewares.ts
+++ src/registerMiddlewares.ts
@@ -1,8 +1,10 @@
+import express from 'express';
 import cookieParserMiddleware from 'cookie-parser';
 import { authenticationMiddleware } from "./middlewares/authenticationMiddleware";
 import { testMiddleware } from "./middlewares/testMiddleware";
 
 export const registerMiddlewares = (app: any) => {
+  app.use(express.json())
   app.use(cookieParserMiddleware());
   app.use(authenticationMiddleware);
   app.use(testMiddleware);
